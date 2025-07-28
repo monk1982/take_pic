33b93a722dfb60303fecc747b9708b7cc91fa4b3
monk1982 <hoangthuc2013@gmail.com>
2025-07-28 22:30:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,90 +1,30 @@
-from flask import Flask, render_template, Response, request, redirect, url_for
-import cv2 as cv
-import threading
-import datetime
-import os
+from flask import Flask, render_template, Response, request, url_for
+import base64
+import cv2
+import numpy as np
 
-global switch, lb_button, capture, img_url
-
-switch=0
-capture=0
-img_path = "static/captures"
-lb_button = 'Bật Camera'
-cur_frame = None
-img_url=None
-frame_lock = threading.Lock()
-
-#make shots directory to save pics
-try:
-    os.mkdir(img_path)
-except OSError as error:
-    pass
-
-#instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
-
-def generate_frame():
-    global cur_frame, capture
-    cam = cv.VideoCapture(0)    
-    while True:
-        success, frame = cam.read()
-        if not success:
-            break
-        with frame_lock:
-            cur_frame = frame.copy()
-
-        _, buffer = cv.imencode('.jpg',frame)
-        frame_bytes = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 # Code hiển thị trình duyệt
 @app.route('/')
 def index():
-    return render_template('index.html', lb_button=lb_button)
+    return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():     
-    return Response(generate_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    data = request.get_json()
+    image_b64 = data['image'].split(',')[1]  # bỏ phần đầu 'data:image/jpeg;base64,...'
 
-@app.route('/capture')
-def capture_image():
-    global cur_frame
-    with frame_lock:
-        if cur_frame is not None:
-            now = datetime.datetime.now()
-            filename = os.path.sep.join([img_path, "capture_{}.png".format(str(now).replace(":",''))])
-            fname =  'captures/'+"capture_{}.png".format(str(now).replace(":",''))
-            cv.imwrite(filename, cur_frame)
-            img_url=url_for('static', filename=fname)
-    return img_url
+    # Decode ảnh từ base64
+    image_bytes = base64.b64decode(image_b64)
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-@app.route('/requests',methods=['POST','GET'])
-def tasks():
-    global switch, lb_button, capture,img_url
-    if request.method == 'POST':
-        if request.form.get('click') == 'Chụp Ảnh':                        
-            img_url = capture_image()
-            switch = 0   
-            capture = 1         
-            lb_button='Bật Camera'                                           
-        else: 
-            if (request.form.get('stop') == 'Bật Camera' ):                        
-                switch = 1
-                capture = 0
-                lb_button='Tắt Camera'                               
-            else:
-                switch = 0
-                capture = 0
-                lb_button='Bật Camera'
+    # Lưu ảnh
+    cv2.imwrite('static/captures/uploaded.jpg', img)
+    print("✅ Đã nhận và lưu ảnh từ client")
 
-    elif request.method=='GET':
-        return render_template('index.html',lb_button=lb_button,capture=capture,img_url=img_url)
-    
-    return render_template('index.html',lb_button=lb_button,capture=capture,img_url=img_url)
+    return "Ảnh đã được gửi lên server!"
 
-# Code chạy lệnh Python
 if __name__ == '__main__':
     app.run()
-        
